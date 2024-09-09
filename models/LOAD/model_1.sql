77f16e0a449b151f64d09489a7b30999d6663990
Mithilesh Dharmale <mithilesh12296@gmail.com>
2024-09-09 17:35:49 +0000

    Third commit

--- models/LOAD/model_1.sql
+++ models/LOAD/model_1.sql
@@ -1,8 +1,21 @@
-with final as (
+with final as
+(
 
-    select * from {{ source('member','table_1') }}
+    select id as Id_1,name as name_1 from {{ source('member','table_1') }}
 
-)
-select * from final
+),
+final_1 as 
+(
 
+    select id as id_2,name as name_2 ,city as city_2
+    
+     from {{ source('member','table_2') }}
+),
+
+final_2 as 
+    (
+    select f.*,f1.* from final f inner join final_1 f1 on f.id_1  = f1.id_2
+    )
+
+select * from final_2
 
